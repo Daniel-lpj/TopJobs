3d6e1baf238fdb998a2d2292c48a8d4374a481da
djunior <djunior@magnasistemas.com.br>
2022-10-16 02:44:29 -0300

    termometro

--- src/Pages/Termometro.jsx
+++ src/Pages/Termometro.jsx
@@ -6,8 +6,7 @@ import ImagemTermometro from "../Images/termometro.png";
 
 const Termometro = () => {
   const titulo = "Termômetro";
-  const descricao =
-    "Na página Termômetro o usuário visualizará seu termômetro entre candidato vaga, ou seja, de acordo com o perfil do candidato, suas experiências profissionais, endereço, cursos e área de formação ele possuirá um nivel de compatibilidade com a empresa e vaga já selecionada na tela de Vagas.";
+  const descricao = "";
   const alt = "Imagem termômetro";
 
   return (
